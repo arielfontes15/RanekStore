a1cd78851cb97e53dd541c278f1549cfc0e66af9
Ariel Vitor <arielfontes15@hotmail.com>
2022-04-18 18:59:41 -0300

    Estruturando nossa service para utilizacao do axios http

--- web-app/src/components/ProdutosLista.vue
+++ web-app/src/components/ProdutosLista.vue
@@ -1,13 +1,37 @@
 <template>
-  <h1>Produtos Lista</h1>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+        <img v-if="produto.fotos" :src="produto.fotos[0].src" alt="produto.fotos[0].titulo">
+        <p class="preco">{{produto.preco}}</p>
+        <h2 class="titulo">{{produto.nome}}</h2>
+        <p>{{produto.descricao}}</p>
+    </div>
+  </section>
 </template>
 
 <script>
+import { api } from '@/services.js';
+
 export default {
-    name: "ProdutosLista"
-}
+  name: "ProdutosLista",
+  data() {
+    return {
+      produtos: null,
+    };
+  },
+  methods: {
+    getProdutos() {
+      api.get("/produto")
+        .then(response => {
+          this.produtos = response.data;
+        }
+      );
+    },
+  },
+  created() {
+    this.getProdutos();
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
