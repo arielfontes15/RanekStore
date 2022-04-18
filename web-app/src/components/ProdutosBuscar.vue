18515ca00613ecfb365decee7a55009ed36a9138
Ariel Vitor <arielfontes15@hotmail.com>
2022-04-18 19:26:51 -0300

    Criando uma abstracao do serialize para busca por produtos

--- web-app/src/components/ProdutosBuscar.vue
+++ web-app/src/components/ProdutosBuscar.vue
@@ -1,13 +1,24 @@
 <template>
-  <input type="text">
+  <div>
+    <input name="busca" id="busca" type="text" v-model="busca" /> 
+    <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos" /> 
+  </div>
 </template>
 
 <script>
 export default {
-    name: "ProdutosBuscar"
-}
+  name: "ProdutosBuscar",
+  data() {
+    return {
+      busca: "",
+    };
+  },
+  methods: {
+      buscarProdutos(){
+          this.$router.push({ query: {q: this.busca}});
+      }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- web-app/src/components/ProdutosLista.vue
+++ web-app/src/components/ProdutosLista.vue
@@ -1,36 +1,51 @@
 <template>
   <section class="produtos-container">
     <div v-for="produto in produtos" :key="produto.id">
-        <img v-if="produto.fotos" :src="produto.fotos[0].src" alt="produto.fotos[0].titulo">
-        <p class="preco">{{produto.preco}}</p>
-        <h2 class="titulo">{{produto.nome}}</h2>
-        <p>{{produto.descricao}}</p>
+      <img
+        v-if="produto.fotos"
+        :src="produto.fotos[0].src"
+        alt="produto.fotos[0].titulo"
+      />
+      <p class="preco">{{ produto.preco }}</p>
+      <h2 class="titulo">{{ produto.nome }}</h2>
+      <p>{{ produto.descricao }}</p>
     </div>
   </section>
 </template>
 
 <script>
-import { api } from '@/services.js';
+import { api } from "@/services.js";
+import { serialize } from "@/helpers.js";
 
 export default {
   name: "ProdutosLista",
   data() {
     return {
       produtos: null,
+      produtosPorPagina: 9
     };
+  },
+  computed: {
+    url() {
+      const query = serialize(this.$route.query)
+      return `/produto?_limit=${this.produtosPorPagina}${query}`;
+    },
+  },
+  watch:{
+    url() {
+      this.getProdutos();
+    }
   },
   methods: {
     getProdutos() {
-      api.get("/produto")
-        .then(response => {
-          this.produtos = response.data;
-        }
-      );
+      api.get(this.url).then((response) => {
+        this.produtos = response.data;
+      });
     },
   },
   created() {
     this.getProdutos();
-  }
+  },
 };
 </script>
 
